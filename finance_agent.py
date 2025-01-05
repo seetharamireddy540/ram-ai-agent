cbb04e2249509a1d18834c76e1e0b04b20cb59cc
mittalas <mittalas@amazon.com>
2025-01-05 11:54:25 -0800

    Initial commit

--- finance_agent.py
+++ finance_agent.py
@@ -2,17 +2,22 @@
 from dotenv import load_dotenv
 from phi.agent import Agent
 from phi.model.groq import Groq
-
+from phi.tools.duckduckgo import DuckDuckGo
+from phi.tools.yfinance import YFinanceTools
 
 # Load environment variables from .env file
 load_dotenv()
 
 print("Hello World")
 
-agent = Agent(
-    model=Groq(id="llama-3.3-70b-versatile")
-)
+finance_agent = Agent(
+    name="Finance Agent",
+    model=Groq(id="llama-3.3-70b-versatile"),
+    tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, company_info=True, company_news=True)],
+    instructions=["Use tables to display data"],
+    show_tool_calls=True,
+    markdown=True,
+    )
 
-agent.print_response(
-    "What is the capital of France?"
-)
+
+finance_agent.print_response("Summarize analyst recommendations for NVDA", stream=True)
